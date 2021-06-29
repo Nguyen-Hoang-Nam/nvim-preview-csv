9349bad12c981220a69ba30130229d5a93c655c4
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-06-29 11:04:54 +0700

    ⚡ Improve performance of preview-csv function
    - This change decrease time to run large csv file
    - By merge for loop and remove same function call in loop

--- lua/nvim-preview.lua
+++ lua/nvim-preview.lua
@@ -1,8 +1,8 @@
 local api = vim.api
 local M = {}
 
-local function print_separate_line(col_length, left, mid, separator, right)
-	local len = #col_length
+local function print_separate_line(col_length, len, left, mid, separator, right)
+	-- local len = #col_length
 	local line = left
 
 	for i = 1, len do
@@ -24,52 +24,57 @@ end
 
 function M.preview_csv()
 	local current_buf = api.nvim_get_current_buf()
-	local filename = api.nvim_eval('expand(\'%:t\')')
+	local filename = api.nvim_eval("expand('%:t')")
 
 	local number_line = api.nvim_buf_line_count(current_buf)
+	if number_line == 0 then
+		return
+	end
+
 	local all_line = api.nvim_buf_get_lines(current_buf, 0, number_line, false)
 	local csv_view = {}
 
-	for _, row in ipairs(all_line) do
-		local line = {}
-		for cell in string.gmatch(row, "[^\",\"]+") do
-			table.insert(line, cell)
-		end
-		table.insert(csv_view, line)
-	end
+	local _, number_columns = string.gsub(all_line[1], ',', '')
+	number_columns = number_columns + 1
 
-	local number_columns = #csv_view[1]
 	local max_col_length = {}
 	for _ = 1, number_columns do
 		table.insert(max_col_length, 0)
 	end
 
-	for _, row in ipairs(csv_view) do
-		for i = 1, #row do
-			if #row[i] > max_col_length[i] then
-				max_col_length[i] = #row[i]
+	for _, row in ipairs(all_line) do
+		local cells = {}
+		local i = 1
+		for cell in string.gmatch(row, '[^","]+') do
+			cell = cell or ''
+			if #cell > max_col_length[i] then
+				max_col_length[i] = #cell
 			end
+			i = i + 1
+
+			table.insert(cells, cell)
 		end
+
+		table.insert(csv_view, cells)
 	end
 
 	local result = {}
-
-	table.insert(result, print_separate_line(max_col_length, '┌', '─', '┬', '┐'))
+	table.insert(result, print_separate_line(max_col_length, number_columns, '┌', '─', '┬', '┐'))
 
 	for _, row in ipairs(csv_view) do
 		if #result > 1 then
-			table.insert(result, print_separate_line(max_col_length, '├', '─', '┼', '┤'))
+			table.insert(result, print_separate_line(max_col_length, number_columns, '├', '─', '┼', '┤'))
 		end
 
-		local line = "│"
+		local line = '│'
 		for i = 1, #row do
-			line = string.format("%s %-" .. max_col_length[i] .. 's │', line, row[i])
+			line = string.format('%s %-' .. max_col_length[i] .. 's │', line, row[i])
 		end
 
 		table.insert(result, line)
 	end
 
-	table.insert(result, print_separate_line(max_col_length, '└', '─', '┴', '┘'))
+	table.insert(result, print_separate_line(max_col_length, number_columns, '└', '─', '┴', '┘'))
 
 	local new_buf = api.nvim_create_buf(true, true)
 	api.nvim_buf_set_name(new_buf, 'Preview ' .. filename)
@@ -78,7 +83,7 @@ function M.preview_csv()
 end
 
 function M.preview()
-	local filetype = api.nvim_eval('expand(\'%:e\')')
+	local filetype = api.nvim_eval("expand('%:e')")
 
 	if filetype == 'csv' then
 		M.preview_csv()
